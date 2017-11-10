0147e08b48981cdadc4354cfc7ef2fe78284c919
Andrew Presnell <andrew_presnell@hotmail.com>
2017-11-10 11:12:21 -0700

    updated port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,5 +29,3 @@ app.use("/", htmlRoutes);*/
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
-
-app.set('port', process.env.PORT || 3000);
